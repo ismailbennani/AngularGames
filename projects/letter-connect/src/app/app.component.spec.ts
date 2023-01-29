0c717c9b29b867bf6ccef0d081496990903965bf
Ismail Bennani <avace.contact@gmail.com>
2023-01-29 19:22:02 +0100

    wip

--- projects/letter-connect/src/app/app.component.spec.ts
+++ projects/letter-connect/src/app/app.component.spec.ts
@@ -15,17 +15,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
-
-    it(`should have as title 'letter-connect'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('letter-connect');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-        expect(compiled.querySelector('.content span')?.textContent).toContain('letter-connect app is running!');
-    });
 });
