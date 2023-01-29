ec08dc11b683ba2b949569a774254fea67279953
Ismail Bennani <avace.contact@gmail.com>
2023-01-29 18:45:36 +0100

    reformat

--- projects/letter-connect/src/app/game/game.service.ts
+++ projects/letter-connect/src/app/game/game.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { createNewGame, GameState } from './engine/game-state';
 import { Observable, ReplaySubject } from 'rxjs';
+import { Engine } from './engine/engine';
 
 @Injectable({
     providedIn: 'root',
 })
 export class GameService {
+    private static readonly LocalStoreKey = 'save';
     private state: GameState;
 
     private stateSubject: ReplaySubject<GameState> = new ReplaySubject<GameState>(1);
@@ -15,11 +17,32 @@ export class GameService {
     }
 
     constructor() {
-        this.state = createNewGame();
+        let state: GameState | undefined;
+
+        const stateStr = localStorage.getItem(GameService.LocalStoreKey);
+        if (stateStr) {
+            state = JSON.parse(stateStr) as GameState;
+        }
+
+        this.state = state ?? createNewGame();
+
         this.notify();
+    }
+
+    public attempt(word: string) {
+        const result = Engine.attempt(this.state, word);
+
+        this.notify();
+
+        return result;
+    }
+
+    public checkGameOver() {
+        return Engine.checkGameOver(this.state);
     }
 
     private notify() {
         this.stateSubject.next(this.state);
+        localStorage.setItem(GameService.LocalStoreKey, JSON.stringify(this.state));
     }
 }
